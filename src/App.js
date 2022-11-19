1494ddf6d1100325e1603327a37b8f8e90841c90
ujjwal <dhakalujjwal99@gmail.com>
2022-11-19 14:49:09 +0545

    added about section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Footer from './components/Footer';
-
+import Progress from './components/Progress';
+import OurTeam from './components/OurTeam';
 
 function App() {
   return (
     <>
-      {/* <Header/>
-      <About/>
-      <Footer/> */}
+      {/* <Header/> */}
+      {/* <About link="https://forms.gle/Nv4MvLyhk8sogRKz6" /> */}
+      {/* <Progress /> */}
+      {/* <OurTeam /> */}
+      {/* <Footer />  */}
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../../src/assets/css/About.css';
+import AboutSectionPhoto from '../../src/assets/img/AboutSectionPhoto.webp'
 
-export default function About() {
+export default function About(props) {
     return (
         <>
-        
-        
-        
-        
+            <section className="container mx-5">
+                <div className="container">
+                    <div className="about-photo">
+                        <img alt="about-photo" src={AboutSectionPhoto} />
+                    </div>
+                    <h1 className="title-font">{props.title}
+                        <p className="text-success">{props.hidden_title}</p>
+                    </h1>
+
+                    <p className="mb-8 text-muted text-left">{props.description}</p>
+                    <span className="joinus-btn">
+                        <a class="btn btn-danger btn-lg" href={props.link} role="button" target="_blank">Join Us</a>
+                    </span>
+                </div>
+            </section>
+
+
+
         </>
     )
+}
+About.protoType={
+    title: PropTypes.string,
+    description: PropTypes.string,
+}
+About.defaultProps={
+    title: 'Want anything to be easy with LaslesVPN.',
+    hidden_title: 'readymade gluten',
+    description: 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.'
 }
