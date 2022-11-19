24089f5de2361a16b345123ccff9695ab312e676
ujjwal <dhakalujjwal99@gmail.com>
2022-11-19 18:54:17 +0545

    updated progress section

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import '../../src/assets/css/Progress.css';
+import PropTypes from 'prop-types';
 
-export default function Progress() {
+export default function Progress(props) {
     return (
-        <>  
-            <section className="text-gray-600 body-font text-center ">
-                <div className="progress_bar">
-                    
-                        <span className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7K</h2>
-                            <p className="leading-relaxed">Users</p>
-                        </span>
-                        <span className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">1.8K</h2>
-                            <p className="leading-relaxed">Subscribes</p>
-                        </span>
-                        <span className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">35</h2>
-                            <p className="leading-relaxed">Downloads</p>
-                        </span>
-                        <span className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">4</h2>
-                            <p className="leading-relaxed">Products</p>
-                        </span>
-                    
+        <>
+            <section class="text-gray-600 body-font">
+                <div class="progress_bar container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4 text-center">
+                        <div class="p-4 sm:w-1/4 w-1/2">
+                            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7K</h2>
+                            <p class="leading-relaxed">Users</p>
+                        </div>
+                        <div class="p-4 sm:w-1/4 w-1/2">
+                            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">1.8K</h2>
+                            <p class="leading-relaxed">Subscribes</p>
+                        </div>
+                        <div class="p-4 sm:w-1/4 w-1/2">
+                            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">35</h2>
+                            <p class="leading-relaxed">Downloads</p>
+                        </div>
+                        <div class="p-4 sm:w-1/4 w-1/2">
+                            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">4</h2>
+                            <p class="leading-relaxed">Products</p>
+                        </div>
+                    </div>
                 </div>
             </section>
-            
+
 
 
         </>
