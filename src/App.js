ef475230f4d4baeeeb68dcab3a81ec3d59218eb8
ujjwalnp <dhakalujjwal99@gmail.com>
2022-11-26 22:16:11 +0545

    updated title/description on About through App.js

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ function App() {
   return (
     <>
       <Header link="https://forms.gle/Nv4MvLyhk8sogRKz6" />
-      <About link="https://forms.gle/Nv4MvLyhk8sogRKz6" />
+      <About link="https://forms.gle/Nv4MvLyhk8sogRKz6" about_title_1="What legacy will you leave behind?" about_title_2="Questions are never indiscreet, Have you asked yourself  on pursuing the ICT field?" about_description="Well if you haven’t already we here at coreQ are present for your answers. We all leave footprints in the sand, the question is, will we be a big heal, or a great soul. Are you satisfied with what you are doing in the field of ICT if not and want yourself to explore more we will be there for you to give you a rollercoaster ride on the technological advancement and exploration. 
+
+To leave the world a little better than you found it join us. "/>
       <Progress />
       <Mission />
       <Programs />
       <OurTeam />
       <Partners />
       <HODMessage />
-      <Footer /> 
+      <Footer />
     </>
   );
 }
